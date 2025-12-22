af07bfd8efa9da00b7b5fce08edd3151b793ec01
Muhammad Umar <muhammad.umar.218980@gmail.com>
2025-12-23 00:22:38 +0500

    Added sample middlewares

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,29 @@ const express = require('express');
 
 const app = express();
 
+let PORT = 5001;
+
 app.get('/', (req, res, next) => {
     res.send('This is the backend server .');
 })
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000 on http://localhost:3000');
+app.get('/api/data', (req, res, next) => {
+    res.status(200).json({message: 'Get request successful!'});
+})
+
+app.post('/api/data', (req, res, next) => {
+    res.status(201).json({message: 'Post request successful!'});
+})
+
+app.put('/api/data', (req, res, next) => {
+    res.status(200).json({message: 'Put request successful!'});
+})
+
+app.delete('/api/data', (req, res, next) => {
+    res.status(200).json({message: 'Delete request successful!'});
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT} on http://localhost:${PORT}`);
 })
