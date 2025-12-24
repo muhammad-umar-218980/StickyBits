54969cf5febbb158138897b8080120c183353e46
Muhammad Umar <muhammad.umar.218980@gmail.com>
2025-12-24 23:20:09 +0500

    Refactor server startup to await DB connection
    Moved connectDB() to ensure the server only starts listening after a successful database connection.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,28 +1,25 @@
-import express from 'express';
-import notesRoutes from './routes/noteRoutes.js';
-import dotenv from 'dotenv';
-import { connectDB } from './config/db.js';
+import express from "express";
+import notesRoutes from "./routes/noteRoutes.js";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 dotenv.config();
 
-const PORT = process.env.PORT || 5001
-
+const PORT = process.env.PORT || 5001;
 
 const app = express();
 
-connectDB();
-
 app.use(express.json());
 
+app.get("/", (req, res, next) => {
+  res.send("Hello This is a simple backend server .");
+});
 
+app.use("/api/notes", notesRoutes);
 
-
-app.get('/', (req, res, next) => {
-    res.send('Hello This is a simple backend server .');
-})
-
-app.use('/api/notes', notesRoutes);
-
-
-app.listen(PORT,()=>{
-    console.log(`\nServer is running on port ${PORT} on http://localhost:${PORT}\n`);
-})
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(
+      `\nServer is running on port ${PORT} on http://localhost:${PORT}\n`
+    );
+  });
+});
