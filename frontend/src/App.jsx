61ff1ea8b1cd7123ed324e0c0a4699d0148709eb
Muhammad Umar <muhammad.umar.218980@gmail.com>
2025-12-26 02:05:24 +0500

    Add Tailwind CSS and routing to frontend
    Integrated Tailwind CSS with DaisyUI by adding configuration files and updating dependencies. Introduced React Router for page navigation and created HomePage, CreatePage, and ViewNote components.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import Navbar  from "./components/Navbar/Navbar"
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import CreatePage from "./pages/CreatePage.jsx";
+import ViewNote from "./pages/ViewNote.jsx";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <Navbar />
+    <div data-theme="night">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<ViewNote />} />
+      </Routes>
     </div>
-  )
-}
-export default App
+  );
+};
+
+export default App;
