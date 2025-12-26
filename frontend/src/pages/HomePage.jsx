e86ef80d1eff1bb40b667a522957770df4b93dd0
Muhammad Umar <muhammad.umar.218980@gmail.com>
2025-12-27 01:05:52 +0500

    Add loading state and toast notifications to HomePage
    HomePage now displays a loading indicator while fetching notes and uses toast notifications to inform the user of success or failure. The notes are set from the API response, and loading state is properly managed.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,23 +1,35 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 const HomePage = () => {
   const [notes, setNotes] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchNotes() {
       try {
         const response = await axios.get("http://localhost:5001/api/notes");
         console.log(response);
+        setNotes(response.data)
+        toast.success("Notes fetched successfully!");
       } catch (error) {
         console.error("Error fetching notes:", error);
+        toast.error("Failed to fetch notes.");
+      }finally{
+        setLoading(false);
       }
     }
 
     fetchNotes();
   }, []);
-  return <div>HomePage</div>;
+
+
+  return (
+    <div>
+      {loading && <div>Loading notes...</div>}
+    </div>
+  );
 };
 
 export default HomePage;
